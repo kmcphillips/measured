af97ebc84645d09b769da4d761569c4776f61a59
Kevin McPhillips <github@kevinmcphillips.ca>
2015-03-02 14:05:24 -0500

    Extract base into own file

--- lib/measured.rb
+++ lib/measured.rb
@@ -1,15 +1,4 @@
-require "measured/version"
-require "active_support"
-require "bigdecimal"
+require "measured/base"
 
-module Measured
-  class UnitError < StandardError ; end
-end
-
-require "measured/measurable"
-require "measured/unit"
-require "measured/conversion"
-require "measured/conversion_table"
-
-require "measured/dimension"
-require "measured/weight"
+require "measured/units/dimension"
+require "measured/units/weight"
---
+++ lib/measured/base.rb
@@ -0,0 +1,12 @@
+require "measured/version"
+require "active_support"
+require "bigdecimal"
+
+module Measured
+  class UnitError < StandardError ; end
+end
+
+require "measured/measurable"
+require "measured/unit"
+require "measured/conversion"
+require "measured/conversion_table"
